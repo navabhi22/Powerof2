4a86b1d554b5d1c4944f10e75242eaacdeb17d8e
navabhi22 <navabhi22@gmail.com>
2022-02-17 18:03:27 +0530

    add class to get Power of two inside int limit

---
+++ PowerOfTwo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerOf2
+{
+    public class PowerOfTwo
+    {
+        public static void PowerOf2(int N)
+        {
+            if (N <31 )
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    double PowOf2 = Math.Pow(2, i);
+
+                    Console.WriteLine(PowOf2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter the value within 0 and 30");
+            }
+            
+        }
+    }
+}
