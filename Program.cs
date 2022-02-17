290ad20b7eacc8b6475d8934b93d356e6439098d
navabhi22 <navabhi22@gmail.com>
2022-02-17 17:59:33 +0530

    adding Program to get power of two

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PowerOf2
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of N ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            PowerOfTwo.PowerOf2(N);
+        }
+    }
+}
